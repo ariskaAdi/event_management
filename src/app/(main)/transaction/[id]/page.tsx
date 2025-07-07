e42e81995de32df5f3ded50fe3d28186bdc07ce0
ariskaAdi <ariskaadiprasetyo@gmail.com>
2025-07-07 23:13:52 +0700

    checkout

--- src/app/(main)/transaction/[id]/page.tsx
+++ src/app/(main)/transaction/[id]/page.tsx
@@ -1,6 +1,24 @@
 import TransactionCard from "@/components/layout/transaction-card";
+import axios from "axios";
 
-export default async function TransactionDetailPage() {
+const getEventById = async ({ params }: { params: { id: string } }) => {
+  try {
+    const { id } = await params;
+    const res = await axios(`${process.env.NEXT_PUBLIC_API_URL}/event/${id}`);
+    console.log(res.data);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default async function TransactionDetailPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const data = await getEventById({ params });
+  const event = data?.event;
   return (
     <main className="min-h-screen flex flex-col">
       <div
@@ -11,11 +29,11 @@ export default async function TransactionDetailPage() {
         <div className="absolute inset-0 bg-black/50"></div>
         <div className="relative container mx-auto px-4 h-full flex flex-col justify-center items-center text-center z-10">
           <div className="flex items-center text-white/80 text-sm">
-            <span>Detail Event</span>
+            <span>Buy Ticket Event</span>
           </div>
         </div>
       </div>
-      <TransactionCard />
+      <TransactionCard event={event} />
     </main>
   );
 }
